7003b5f4287f0268055b304ccc86950ad810127e
mdraganov <mariyandraganov@gmail.com>
2016-02-24 11:08:51 +0200

    Added Message.cs model.

---
+++ Web/AllegroExtended.Web/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿namespace AllegroExtended.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Infrastructure.Mapping;
+    using Services.Data;
+
+    public class MessageController : BaseController
+    {
+        public MessageController()
+        {
+        }
+
+        public ActionResult All()
+        {
+            return this.View();
+        }
+    }
+}
